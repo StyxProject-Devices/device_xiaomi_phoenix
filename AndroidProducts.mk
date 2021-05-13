735dec778d1b0472c6f8ca2650601e47ed45e780
Rasenkai <rasenkai99@gmail.com>
2021-05-13 11:19:18 +0000

    phoenix: styx bringup
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_phoenix.mk
+    $(LOCAL_DIR)/styx_phoenix.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_phoenix-user \
-    lineage_phoenix-userdebug \
-    lineage_phoenix-eng
+    styx_phoenix-user \
+    styx_phoenix-userdebug \
+    styx_phoenix-eng
--- styx_phoenix.mk
+++ styx_phoenix.mk
@@ -11,11 +11,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from phoenix device
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common STYX stuff.
+$(call inherit-product, vendor/styx/config/common.mk)
 
-PRODUCT_NAME := lineage_phoenix
+PRODUCT_NAME := styx_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
 PRODUCT_MANUFACTURER := Xiaomi
+
+#STYX PROPERTIES
+STYX_BUILD_VARIANT := OFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
