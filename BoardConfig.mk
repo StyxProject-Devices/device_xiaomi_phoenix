f0375a8402a9141c9d63e1f7604412623d415e1d
Gagan Malvi <malvigagan@gmail.com>
2022-06-07 10:04:25 +0200

    phoenix: Move to new defconfig.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -131,15 +131,12 @@ BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0x8800
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image.gz
-TARGET_KERNEL_CONFIG := vendor/phoenix_defconfig
+TARGET_KERNEL_CONFIG := phoenix_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/phoenix
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-
-TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # Media
 TARGET_DISABLED_UBWC := true
